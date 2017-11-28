69329b94012d5fbef0cdc8ec9317ac6d1683e004
Roman Dorosinets <mr.dorosinec@mail.ru>
2017-11-28 19:40:00 +0300

    Write simple server

--- Server/src/by/bsu/chat/server/ChatServer.java
+++ Server/src/by/bsu/chat/server/ChatServer.java
@@ -1,8 +1,61 @@
 package by.bsu.chat.server;
 
-public class ChatServer {
+import by.bsu.network.TCPConnection;
+import by.bsu.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnectionListener {
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
 
     public static void main(String[] args) {
+        new ChatServer();
+    }
 
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    // TODO: 28.11.2017 TCPConnection + e
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnection("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnection(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);// TODO: 28.11.2017 TCPConnection + e LOG
+    }
+
+    private void sendToAllConnection(String value) {
+        System.out.println(value);
+        for (TCPConnection connection : connections) {
+            connection.sendString(value);
+        }
     }
 }
